3d847566005223504cfd1b682ee50915b3076698
MolarFox <MolarFox@outlook.com>
2017-07-12 13:00:53 +1000

    Added primary interface unit code

---
+++ Interface Code/Sensors-in_no-touch/Sensors-in_no-touch.ino
@@ -0,0 +1,24 @@
+/*  Sensor Visualisation code (no touch used)
+ *  C: 12/07/2017 | LM: 12/07/2017
+ *  ----------------------------------
+ *  Reads in sensor info via serial from command unit and outputs to screen
+ *  Touchscreen hardware unreliable - not using touch
+ *  ----------------------------------
+ *  By Rithesh R Jayaram
+ *  ----------------------------------
+ *  Based on modified Adafruit tftPaint sketch
+ *  Using modified Adafruit touch and tft display libraries
+*/
+
+// LIBRARIES //
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_TFTLCD.h> // Hardware-specific library
+//#include <TouchScreen.h>    // Touchscreen library
+
+void setup() {
+  
+}
+
+void loop() {
+  
+}
