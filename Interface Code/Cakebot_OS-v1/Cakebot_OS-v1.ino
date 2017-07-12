d067bd4f49d1219a894ce868be91e0d135c4bcd2
Rithesh Jayaram <rrjayaram@outlook.com.au>
2017-07-12 21:32:38 +1000

    Added battery determination widget to screen

--- Interface Code/Cakebot_OS-v1/Cakebot_OS-v1.ino
+++ Interface Code/Cakebot_OS-v1/Cakebot_OS-v1.ino
@@ -24,12 +24,17 @@
 Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET); // Initialise TFT LCD instance
 
 // Common Colour Codes //
-#define THEMECOLOUR 0x07E0
-#define RED         0xF800
+#define ORANGE      0xFF00
+#define YELLOW      0xFFE0
+#define GREEN       0x07E0
+#define RED         0xF826
 #define WHITE       0xFFFF
 #define BLACK       0x0000
+#define PINK        0xF8FA
 
-
+// Battery voltage levels (adjust as necessary)
+#define lowVolt     7.0
+#define highVolt    8.4
 
 void setup() {
   tft.reset();
@@ -38,12 +43,22 @@ void setup() {
   tft.fillScreen(BLACK);
   tft.setRotation(3);
 
+  drawMainWindow("    - COMD MODULE MONITOR -",0xFFE0, GREEN);  // Draws menubar and screen borders
+}
+
+void loop() {
+  
+}
+
+// Function to draw outer screen borders and menubar (Window title, title colour, border colour)
+void drawMainWindow(String winTitle, int winTitleColour, int BorderColour){
   // Screen (window) borders
-  tft.drawRect(0, 0,  320, 16,  THEMECOLOUR);
-  tft.drawRect(0, 15, 320, 225, THEMECOLOUR);
+  tft.fillRect(0, 0,  320, 16,  BLACK);
+  tft.drawRect(0, 0,  320, 16,  BorderColour);
+  tft.drawRect(0, 15, 320, 225, BorderColour);
 
   // Titlebar dividers
-  tft.drawLine(19, 0, 19, 15, THEMECOLOUR);
+  tft.drawLine(19, 0, 19, 15, BorderColour);
 
   // Team CI Icon
   tft.setCursor(5, 4);
@@ -55,13 +70,62 @@ void setup() {
   tft.setCursor(23, 4);
   tft.setTextColor(WHITE);  tft.setTextSize(1);
   tft.print("CAKEBOT OS v1.0");
-  tft.setTextColor(0xFFE0);  tft.setTextSize(1);
-  tft.println("    - COMD MODULE VALS -");
+  tft.setTextColor(winTitleColour);  tft.setTextSize(1);
+  tft.println(winTitle);
 
-  // Battery icon (TODO: Remove)
-  tft.drawRect(292, 2, 25, 11, RED);
+  drawBatteryIcon();
 }
 
-void loop() {
-  
+// Function to measure then display battery levels on menubar (Do not call function too quickly)
+void drawBatteryIcon(){
+  float battLevel = getcurrVcc();
+  float battPercent;
+  int LEVELCOLOUR;
+  if (battLevel < 540){ // If battery is less than 5.4v, likely connected to USB supply, not battery
+    tft.setCursor(296, 4);
+    tft.setTextColor(GREEN);  tft.setTextSize(1);
+    tft.print("PWR"); // Write inside battery instead of filling
+    
+    tft.drawRect(292, 2, 25, 11, GREEN);  // Draw battery
+    tft.drawRect(289, 4, 4, 6, GREEN);    // Draw battery nib
+
+    //TEMP
+    tft.fillRect(293, 3, 23, 9, RED);
+    
+  }else{  // Battery is connected to VCC in
+    battPercent = (((battLevel/100) - lowVolt)/(highVolt-lowVolt))*100; // Determine battery percentage based on expected voltages
+    
+    // Determine colour for battery level
+    if (battPercent > 75) LEVELCOLOUR = GREEN;
+    else if (battPercent > 50) LEVELCOLOUR = ORANGE;
+    else if (battPercent > 25) LEVELCOLOUR = YELLOW;
+    else LEVELCOLOUR = RED;
+
+    //Draw icon fill
+    int fillAmount = 9*round((BattPercent/100));  // Find length of battery bar based on percentage
+    tft.fillRect(316-fillAmount, 3, fillAmount, 9, LEVELCOLOUR);
+    
+    // Draw icon outline
+    tft.drawRect(292, 2, 25, 11, LEVELCOLOUR+1);    // Draw battery
+    if (battPercent > 95) tft.fillRect(289, 4, 4, 6, GREEN);  // Draw battery nib
+    else tft.drawRect(289, 4, 4, 6, GREEN); // Fill battery nib
+  }
 }
+
+// Voltage determination function (By RetroLefty - Arduino Forums)
+int getcurrVcc(void){ // Returns actual value of Vcc (x 100) Volts
+    // For 168/328 boards
+    const long InternalReferenceVoltage = 1056L;            // Adjust this value to your boards specific internal BG voltage x1000
+       // REFS1 REFS0          --> 0 1, AVcc internal ref.  -Selects AVcc external reference
+       // MUX3 MUX2 MUX1 MUX0  --> 1110 1.1V (VBG)          -Selects channel 14, bandgap voltage, to measure
+    ADMUX = (0<<REFS1) | (1<<REFS0) | (0<<ADLAR) | (1<<MUX3) | (1<<MUX2) | (1<<MUX1) | (0<<MUX0);
+     
+    delay(0.5);
+       // Start a conversion  
+    ADCSRA |= _BV( ADSC );
+       // Wait for it to complete
+    while( ( (ADCSRA & (1<<ADSC)) != 0 ) );
+       // Scale the value
+    int results = (((InternalReferenceVoltage * 1024L) / ADC) + 5L) / 10L; // calculates for straight line value
+    return results;
+   }
