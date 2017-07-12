aebb8aedc5b2876e5e3903f0a68edffef2a8ed51
Rithesh Jayaram <rrjayaram@outlook.com.au>
2017-07-12 14:01:44 +1000

    Basic graphics working

--- Interface Code/Sensors-in_no-touch/Sensors-in_no-touch.ino
+++ Interface Code/Sensors-in_no-touch/Sensors-in_no-touch.ino
@@ -15,8 +15,48 @@
 #include <Adafruit_TFTLCD.h> // Hardware-specific library
 //#include <TouchScreen.h>    // Touchscreen library
 
+// Pin Definitions //
+#define LCD_CS A3
+#define LCD_CD A2
+#define LCD_WR A1
+#define LCD_RD A0
+#define LCD_RESET A4
+Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET); // Initialise TFT LCD instance
+
+// Common Colour Codes //
+#define THEMECOLOUR 0x07E0
+#define RED         0xF800
+#define WHITE       0xFFFF
+#define BLACK       0x0000
+
+
+
 void setup() {
-  
+  tft.reset();
+  uint16_t identifier = tft.readID(); // Gets driver number from device
+  tft.begin(identifier);  // Inits for detected device
+  tft.fillScreen(BLACK);
+  tft.setRotation(3);
+
+  // Screen (window) borders
+  tft.drawRect(0, 0,  320, 16,  THEMECOLOUR);
+  tft.drawRect(0, 15, 320, 225, THEMECOLOUR);
+
+  // Titlebar dividers
+  tft.drawLine(19, 0, 19, 15, THEMECOLOUR);
+
+  // Team CI Icon
+  tft.setCursor(5, 4.5);
+  tft.setTextColor(0x07FF);  tft.setTextSize(1);
+  tft.print("CI");
+  tft.drawRect(3, 3.5, 14, 11, 0xF81F);
+
+  // Title Bar text
+  tft.setCursor(23, 4);
+  tft.setTextColor(WHITE);  tft.setTextSize(1);
+  tft.print("CAKEBOT OS v1.0");
+  tft.setTextColor(RED);  tft.setTextSize(1);
+  tft.println("  - DIAGNOSTICS -");
 }
 
 void loop() {
