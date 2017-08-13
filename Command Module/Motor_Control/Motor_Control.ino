0d1f68f313979367abeff5f274660884d9bcaad5
Rithesh Jayaram <rrjayaram@outlook.com.au>
2017-08-13 15:45:08 +1000

    Local angle now subdivided into sectors

--- Command Module/Motor_Control/Motor_Control.ino
+++ Command Module/Motor_Control/Motor_Control.ino
@@ -12,35 +12,51 @@
 #include <Wire.h> // I2C used only for magnetometer
 #include <Adafruit_Sensor.h>
 #include <Adafruit_HMC5883_U.h>
-
 Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345); // Instantiate magnetometer
+
+// STATIC VARIABLES //
+#define magReadDelay 2  // Minimum delay between magnetometer readings (milliseconds)
+// Pinouts of motors:    1CCW 1CW | 2CCW 2CW | 3CCW 3CW
+const int drv[6] =       {2,   3,    4,   5,    6,   7};
 
 // VARIABLES //
 byte rotConType= 0;     // Current rotation mode (0 = auto, 1 = local, 2 = global)
 byte driveConType = 0;  // Current drive    mode (0 = auto, 1 = local, 2 = global)
 
+float milHeading;       // Raw value (in millirads) from magnetometer
 int locRotAngle, globRotAngle, locDriveAngle, globDriveAngle; // Values for control vectors
 
-signed int M1, M2, M3;  // Motor drive values
+float ratM1, ratM2, ratM3;  // Motor drive ratios
+float M1, M2, M3;           // Raw motor drive values (%)
+int maxDrive;               // Value of most activated motor (%)
 
 
 void setup() {
   mag.begin(); // Initialise magnetometer
+  Serial.begin(9600); // TEMP
 }
 
 void loop() {
   relaxMag(); // Refreshes current mag readings
+  Serial.println(milHeading); // TEMP
+  
 }
 
+// Sets 
 
 // Relaxes information from magnetometer
 void relaxMag(void){
-  float heading;    // Holds heading value in radians
-  float milHeading; // Holds heading value in mils
+  float heading;          // Holds heading value in radians
+  static float nextRun;   // Holds next runtime (millis)
   
-  sensors_event_t event; // Gets new magnetometer event
-  mag.getEvent(&event);
-  heading = atan2(event.magnetic.y, event.magnetic.x);  // Store new vals in variable (does not account for tilt)
-  milHeading = heading * 180/M_PI;
+  if (millis() > nextRun){
+    sensors_event_t event; // Gets new magnetometer event
+    mag.getEvent(&event);
+    heading = atan2(event.magnetic.y, event.magnetic.x);  // Store new vals in variable (does not account for tilt)
+    milHeading = heading * 1000;            // Convert to milliradians for global variable
+    if (milHeading < 0) milHeading += 6400; // Compensate for negative reading
+    
+    nextRun = millis() + magReadDelay;  // Schedule next refresh
+  }
 }
 
