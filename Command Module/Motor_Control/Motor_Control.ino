17c5b1bb7aecf5d25e3bd31f4581dad3a478c38d
Rithesh Jayaram <rrjayaram@outlook.com.au>
2017-08-13 14:16:52 +1000

    Added motor control sketch

---
+++ Command Module/Motor_Control/Motor_Control.ino
@@ -0,0 +1,20 @@
+/*  Motor Control Sketch
+ *  C: 13/08/2017 | LM: 13/08/2017
+ *  ----------------------------------
+ *  NB: This sketch needs to be converted to library asap
+ *  ----------------------------------
+ *  By Rithesh R Jayaram
+ *  ----------------------------------
+*/
+
+// Variables //
+byte rotConType= 0;     // Current rotation mode (0 = auto, 1 = local, 2 = global)
+byte driveConType = 0;  // Current drive    mode (0 = auto, 1 = local, 2 = global)
+
+void setup() {
+  
+}
+
+void loop() {
+  
+}
