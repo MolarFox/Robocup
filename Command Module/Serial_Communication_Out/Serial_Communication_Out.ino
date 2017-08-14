92ded0f2415b8b5a5b3124e0deb3f45d848f5252
Rithesh Jayaram <rrjayaram@outlook.com.au>
2017-08-14 13:41:08 +1000

    Added TRIS10 IR Sensor diagnostic

---
+++ Command Module/Serial_Communication_Out/Serial_Communication_Out.ino
@@ -0,0 +1,21 @@
+/*  <SKETCH TITLE>
+ *  C: / / | LM: / /
+ *  ----------------------------------
+ *  <Description>
+ *  ----------------------------------
+ *  By Rithesh R Jayaram
+ *  ----------------------------------
+ *  Based on ___
+ *  Using ___
+*/
+
+// LIBRARIES //
+
+
+void setup() {
+  
+}
+
+void loop() {
+  
+}
