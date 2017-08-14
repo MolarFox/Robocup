5b90e63afe6c6b054be2e5cc7d2b2052081dfdce
Rithesh Jayaram <rrjayaram@outlook.com.au>
2017-08-14 15:44:07 +1000

    Added stopgap code for state comp
    Motor 2 is non-responsive

---
+++ Command Module/StateComp_Stopgap-Code/StateComp_Stopgap-Code.ino
@@ -0,0 +1,20 @@
+/*  Robocup State Competition code
+ *  C: 14/08/2017| LM: 14/08/2017
+ *  ----------------------------------
+ *  <Description>
+ *  ----------------------------------
+ *  By Rithesh R Jayaram
+ *  ----------------------------------
+ *  Based on Adafuit HMC885l library and TRIS10 TSOP31140 code
+*/
+
+// LIBRARIES //
+
+
+void setup() {
+  
+}
+
+void loop() {
+  
+}
